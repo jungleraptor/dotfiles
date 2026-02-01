10e373a5b94377d23e262f42d9bacd830db017a0
Isaac Torres <isaac@isaactorz.dev>
2026-02-01 15:16:12 -0800

    fish: modify PATH order and clean up junk
    Most importantly puts ~/.local/bin in the path in front of homebrew
    so those binaries are preferred.
    
    Also cleans up a bunch of junk that's outdated or from previous
    jobs.

--- config.fish
+++ config.fish
@@ -1,28 +1,10 @@
-# for npm to work right
-# see https://stackoverflow.com/questions/52979927/npm-warn-checkpermissions-missing-write-access-to-usr-local-lib-node-modules
-set PATH $HOME/.cargo/bin $PATH
-
-# mainly for haskell
-set PATH $HOME/.local/bin $PATH
-
-set PATH $HOME/bin $PATH
-
 set PATH /home/linuxbrew/.linuxbrew/bin $PATH
 
-set -x GHUSER isaactorz
-
-set -x USE_GKE_GCLOUD_AUTH_PLUGIN True
+set PATH $HOME/.local/bin $PATH
 
 # fzf
 set -x FZF_DEFAULT_COMMAND "rg --files --hidden --follow --glob '!.git'"
 set -x FZF_DEFAULT_OPTS "--height 40% --reverse"
-
-# cmake defaults
-# set -x CMAKE_C_COMPILER_LAUNCHER sccache
-# set -x CMAKE_CXX_COMPILER_LAUNCHER sccache
-# set -x CMAKE_GENERATOR Ninja
-# 
-# set -x BAZEL_PROFILE buildbarn
 
 # aliases
 alias vim=nvim
@@ -37,8 +19,6 @@ alias cat=bat
 
 # starship
 starship init fish | source
-
-set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin /home/parallels/.ghcup/bin $PATH # ghcup-env
 
 # try to source a machine specific config
 if test -e ~/.local.fish
